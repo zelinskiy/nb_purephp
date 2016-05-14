d9387b3d8f0f43d15d128c0c027f65c30ee95953
zelinskiy <zelinskiy1917@gmail.com>
2016-05-14 23:38:59 +0300

    Local bootstrap.js fixed

--- notes.php
+++ notes.php
@@ -20,7 +20,7 @@ if(!isset($_SESSION["userid"])){
 	<script src="/js/jquery.js"></script>
 
 
-	<!--script src="js/bootstrap.js"></script-->
+	<script src="js/bootstrap.js"></script>
 
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
